997272552279a4780ffa696ebc73d7a57d34882c
Marie-Amélie Gilles <gillesmarieamelie@gmail.com>
2026-08-25 14:08:24 +0200

    Update config/routes.rb
    Co-authored-by: Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get "claims/index"
-  get "claims/show"
-  get "claims/new"
-  get "claims/create"
-  get "claims/destroy"
-  get "claims/update"
+    resources :claims
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
