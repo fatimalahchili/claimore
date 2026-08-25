619139f94dac04568ab1cd3818a9293e739cb733
CyrilLagnien <lagniencyril@gmail.com>
2026-08-25 12:44:40 +0200

    fatou new corrections

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,18 +1,23 @@
 class TemplatesController < ApplicationController
+  before_action :set_template, only: [:show, :edit]
+
   def index
     @templates = Template.all
   end
 
   def show
-    @template = Template.find(params[:id])
   end
 
   def new
     @template = Template.new
   end
 
-
   def edit
+  end
+
+  private
+
+  def set_template
     @template = Template.find(params[:id])
   end
 end
