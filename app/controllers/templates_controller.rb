9638aa898b5dc6f9516edce810af4a08a317441c
CyrilLagnien <lagniencyril@gmail.com>
2026-08-25 12:18:14 +0200

    fatima corrections

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,13 +1,18 @@
 class TemplatesController < ApplicationController
   def index
+    @templates = Template.all
   end
 
   def show
+    @template = Template.find(params[:id])
   end
 
   def new
+    @template = Template.new
   end
 
+
   def edit
+    @template = Template.find(params[:id])
   end
 end
