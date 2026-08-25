1fbd05f577b39775f087b07b8b3896c778d81811
CyrilLagnien <lagniencyril@gmail.com>
2026-08-25 11:40:56 +0200

    templates

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,13 @@
+class TemplatesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_24_130747) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_25_091804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "templates", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
