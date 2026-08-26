c0ddca7578e52081c14ae8ec092601e6dbc0422a
CyrilLagnien <lagniencyril@gmail.com>
2026-08-26 11:13:49 +0200

    timeline-mvc-for-claims-model

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -2,6 +2,11 @@ class ClaimsController < ApplicationController
   def index
   end
 
+  def timeline
+    @claim = Claim.find(params[:claim_id])
+    @entries = @claim.entries
+  end
+
   def show
   end
 
