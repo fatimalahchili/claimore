39e2d29980d369096e2e2bd41f0dd931241e1c2d
blesson paul <blempaul@gmail.com>
2026-08-27 16:43:03 +0200

    Build claim show page with chronological timeline linking to entries (#75)

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,24 +1,55 @@
 class ClaimsController < ApplicationController
-  def index
-  end
+  before_action :set_claim, only: %i[show edit update destroy timeline]
 
-  def timeline
-    @claim = Claim.find(params[:claim_id])
-    @entries = @claim.entries.order(:created_at)
+  def index
+    @claims = Claim.all
   end
 
   def show
+    @entries = @claim.entries.order(date: :desc, created_at: :desc)
+  end
+
+  def timeline
+    @entries = @claim.entries.order(date: :desc, created_at: :desc)
   end
 
   def new
+    @claim = Claim.new
   end
 
   def create
+    @claim = Claim.new(claim_params)
+    if @claim.save
+      redirect_to @claim, notice: "Claim was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def destroy
+  def edit
   end
 
   def update
+    if @claim.update(claim_params)
+      redirect_to @claim, notice: "Claim was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @claim.destroy
+    redirect_to claims_path, notice: "Claim was successfully deleted."
+  end
+
+  private
+
+  def set_claim
+    @claim = Claim.find(params[:id] || params[:claim_id])
+  end
+
+  def claim_params
+    params.require(:claim).permit(:category, :status, :property_id)
   end
 end
+
