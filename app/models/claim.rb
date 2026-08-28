b056d7593de78a6721913792d92bf7dde978bc93
Thomas <thomasdanielrice@proton.me>
2026-08-28 15:16:43 +0200

    fixing again

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -7,5 +7,5 @@ class Claim < ApplicationRecord
   has_many :users, through: :tenants
   has_many :chats, dependent: :destroy
 
-  enum :role, { archived: "archived", active: "active" }, default: :guest
+  enum :status, { archived: "archived", active: "active" }, default: :active
 end
