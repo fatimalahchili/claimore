9e166e84cb55f9ce818a2324257c533057b6cf03
Thomas <thomasdanielrice@proton.me>
2026-08-31 10:30:36 +0200

    moved chat into application html.erb

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ApplicationRecord
   acts_as_chat
-  belongs_to :claim
+  belongs_to :claim, optional: true
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_28_114014) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_31_080547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_28_114014) do
   end
 
   create_table "chats", force: :cascade do |t|
-    t.bigint "claim_id", null: false
+    t.bigint "claim_id"
     t.datetime "created_at", null: false
     t.bigint "model_id"
     t.datetime "updated_at", null: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_28_114014) do
     t.string "category"
     t.datetime "created_at", null: false
     t.bigint "property_id", null: false
-    t.string "status"
+    t.string "status", default: "active"
     t.datetime "updated_at", null: false
     t.index ["property_id"], name: "index_claims_on_property_id"
   end
